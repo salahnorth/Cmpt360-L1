c6de1b8ce2384f2b8838709758c5ab5b49de17bf
Vy Tran <tranv33@mymacewan.ca>
2024-03-20 16:29:19 -0600

    Everything worked for Q1 (logic part)

--- lab09/testfindme.c
+++ lab09/testfindme.c
@@ -7,27 +7,35 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 6) {
-        printf("Usage: %s <starting directory> <file type> <maxdepth> <username> <file name>\n", argv[0]);
+        printf("Usage: %s <starting directory> <file type> <file name> <user name> <maxdepth>\n", argv[0]);
         return 1;
     }
 
     char *path = argv[1];
-    int filetype = atoi(argv[2]);
+    char* filetype = argv[2];
+ 
     int maxdepth = atoi(argv[5]);
     char *username = argv[4];
     char *filename = argv[3];
+    int fType;
+	printf("\n\n\n\n\n\n\nFILE TYPE IN TEST %s\n", filetype);
 	
-    int res;
-    /*res = readFromFile(path, filetype, filename, username, maxdepth);
-    printf("FILE NAME FROM MAIN %s\n", filename);
-    printf("RESULT FROM readFromFile %i\n", res);
-    if(res == 0){
-	printf("NOTHING!\n");
-    }
-    else{
-	    printf("MATCHED! FILE FOUND!\n");
+	if(*filetype == 'f'){
+		fType = 0;
+	}
+	else if(*filetype == 'd'){
+		fType = 1;
+	}
+	else if(*filetype == 'c'){
+		fType = 2;
+	}
+	else if(*filetype == 'b'){
+		fType = 3;
+	}
+	else if(*filetype == 's'){
+		fType = 4;
+	}
 
-    }*/
-    readFromFile(path, filetype, filename, username, maxdepth);
+    readFromFile(path, fType, filename, username, maxdepth);
     return 0;
 }
