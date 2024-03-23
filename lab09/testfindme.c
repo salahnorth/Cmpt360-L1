27a121f76c1649f1c2df90e8032067ca6c8d08b9
Vy Tran <tranv33@mymacewan.ca>
2024-03-23 12:23:56 -0600

    Added input path modification

--- lab09/testfindme.c
+++ lab09/testfindme.c
@@ -5,6 +5,9 @@
 #include<stdlib.h>
 #include "findme.h"
 
+
+void replace(char*, char*);
+
 int main(int argc, char *argv[]) {
     if (argc != 6) {
         printf("Usage: %s <starting directory> <file type> <file name> <user name> <maxdepth>\n", argv[0]);
@@ -20,6 +23,16 @@ int main(int argc, char *argv[]) {
     int fType;
 	printf("\n\n\n\n\n\n\nFILE TYPE IN TEST %s\n", filetype);
 	
+	//int flag = 0;
+	char c1 = '/';
+	char* pt;
+	pt = &c1;
+	
+	
+	replace(path, pt); 
+	printf("Changing path %s\n", path);
+
+
 	if(*filetype == 'f'){
 		fType = 0;
 	}
@@ -38,4 +51,18 @@ int main(int argc, char *argv[]) {
 
     readFromFile(path, fType, filename, username, maxdepth);
     return 0;
+}
+
+
+void replace(char* s, char* c){
+	int counter = 0;
+	for(int i=0; s[i] != '\0'; i++){
+		printf("GOING IN FOR\n");
+		if(s[i] != *c){
+			continue;
+		}
+		if(s[strlen(s) - 1] == *c){
+			s[strlen(s) -1] = '\0';
+		}
+	}
 }
