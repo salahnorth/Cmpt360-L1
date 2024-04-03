8b9c227c9b6a3e8385f39470fb8bd8c58aaf1284
Vy Tran <tranv33@mymacewan.ca>
2024-04-02 18:14:28 -0600

    Final Version of lab10 (tested on std server)

--- lab10/findme.c
+++ lab10/findme.c
@@ -8,146 +8,138 @@
 #include<pwd.h>
 #include<sys/wait.h>
 #include"findme.h"
-
 #include<glob.h>
-
+#include<regex.h>
 
 /**
   *@brief Searching for a specific file
   *@param path, filetype, filename, username, maxdepth
   *@return 0 or 1
   */
-int findFile(char *path, int filetype,char* filename,char* username, int maxdepth){
-
+int findFile(char *path, int filetype, char* filename, char* username, int maxdepth) {
+    // Flag to identify if it is a root directory
     int rootFlag = 0;
-    
     struct dirent *entry;
     DIR *dp;
-    
     struct stat statbuf;
-    int res;
-    int res2;
+
+    // Open the directory
     dp = opendir(path);
-    
-    if(dp == NULL){
+
+    // Check if directory can be opened
+    if(dp == NULL) {
         perror("opendir");
         return 0;
     }
-    
-    if(stat(path, &statbuf) == -1){
-         perror("stat failed");
-         exit(EXIT_FAILURE);
-         return 0;
-     }
-            	    
-    while((entry = readdir(dp))){
-        if(stat(path, &statbuf) == -1)
-            continue;
-    
-        FILE *fp1;
 
-	if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
-	    continue;
-	}
-	
-	char *filePath = (char*) malloc((strlen(path)+1) + (strlen(entry->d_name)+1));
-	
-	filePath = strcpy(filePath, path);
-	
-	if (strcmp(filePath, "/") == 0){
-	     rootFlag = 1;
-	}
-	
-	if (rootFlag == 0){
-	    strcat(filePath, "/");
-	    strcat(filePath, entry->d_name);
-	}
-	else if(rootFlag != 0){
-	    strcat(filePath, entry->d_name);
-	}
-	
-	int filetypeFromTest = fileSystemTests(filePath);
-	
-	if(maxdepth > 0){
-	    if(filetypeFromTest == 1){
-	        pid_t pid = fork();
-	        if (pid == -1){
-	            perror("fork");
-	            exit(EXIT_FAILURE);
-	            
-	        }
-	
-	        else if (pid == 0){
-			//		if(res ==0){
-				findFile(filePath,filetype,filename, username, maxdepth - 1);
-	            		closedir(dp);
-	            		exit(EXIT_SUCCESS);
-			//}
-			//else{
-			//	findFile(filePath,filetype,filename, username, maxdepth - 1);
-			//}
-			//closedir(dp);
-	        }
-	    
-	    }
-	    if(filetype == filetypeFromTest || filetype == DEFAULT_FILETYPE){
-	    
-	        fp1 = fopen(filePath, "r");
-
-	        if(fp1 == NULL){
-	            continue;
-	        }
-	        else{
-	            struct stat fileStat;
-                    if (stat(filePath, &fileStat) == 0){
-                        struct passwd *pwd = getpwuid(fileStat.st_uid); 
-                        if(pwd != NULL && strcmp(username, pwd->pw_name) == 0){
-				//strcat(filePath, "/");
-				//strcat(filePath, filename);
-
-				printf("FILE PATH WITH WILDCARD %s\n", filePath);
-				res = globFunct(filePath);
-				//res2 = globFunct(filename);
-				if(res == 0){
-					printf("ENTRY D_NAME %s\n",entry->d_name);
-					printf("FILE NAME %s\n", filename);
-                            		//if(strcmp(entry->d_name, filename) == 0 || strcmp(filename, "") == 0){       	
-                                		printPath(filePath);
-			        		free(filePath);
-					//}
-					
-			    }
-
-			
-                        }
-                      
-                        
-                        else{
-			    printf("Can't access, wrong username\n");
-			    return 1;
-	                }
-                    }
-                    
-                    else{
-	                printf("Can't access, stat file error\n");
-	                return 1;
-	                
-	            }
-
-                    fclose(fp1);
-	        }
-	        
-	    }
-
-        }
-        
+    // Get the status of the path
+    if(stat(path, &statbuf) == -1) {
+        perror("stat failed");
+        exit(EXIT_FAILURE);
+        return 0;
     }
-	
+
+    // Loop through the entries in the directory
+    while((entry = readdir(dp))) {
+        FILE *fp1;
+        char *filePath = (char*) malloc((strlen(path)+1) + (strlen(entry->d_name)+1));
+        filePath = strcpy(filePath, path);
+
+        if (strcmp(filePath, "/") == 0) {
+            rootFlag = 1; // Constructing the filepath without / at the end for root directory
+        }
+
+        if (rootFlag == 0) {
+            strcat(filePath, "/");
+            strcat(filePath, entry->d_name);
+        }
+        else if(rootFlag != 0) {
+            strcat(filePath, entry->d_name);
+        }
+
+        // Skip hidden files
+        if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        // Obtain file type from a file
+        int filetypeFromTest = fileSystemTests(filePath);
+
+        // Recursively search subdirectories
+        if (filetypeFromTest == 1 && maxdepth > 0) {
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            else if (pid == 0) {
+                findFile(filePath, filetype, filename, username, maxdepth - 1);
+                closedir(dp);
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+        // Print out the matched filepaths
+        if(filetype == filetypeFromTest || filetype == DEFAULT_FILETYPE) {
+            fp1 = fopen(filePath, "r");
+            if(fp1 == NULL) {
+                continue;
+            }
+            else {
+                glob_t gStruct;
+
+                int glob_res = glob(filePath, GLOB_NOESCAPE, NULL, &gStruct);
+                
+                struct stat fileStat;
+                if (stat(filePath, &fileStat) == 0){
+                    struct passwd *pwd = getpwuid(fileStat.st_uid);
+                        
+                    if (pwd != NULL && strcmp(username, pwd->pw_name) == 0) {
+                
+                        if (glob_res == 0) {
+                            regex_t regex;                                                                    
+                            if (regcomp(&regex, filename, REG_EXTENDED) == 0) {
+                                      
+                            for (int i = 0; i < gStruct.gl_pathc; i++) {
+                                if (regexec(&regex, gStruct.gl_pathv[i], 0, NULL, 0) == 0) {
+
+                                    printPath(gStruct.gl_pathv[i]);
+                                }
+                            }
+                        
+                            regfree(&regex);
+                        } else {
+                                perror("regcomp failed");
+                                
+                                globfree(&gStruct);
+                                free(filePath);
+                                
+                                closedir(dp);
+                                return -1;
+                            }
+
+                                globfree(&gStruct);
+
+                        } else {
+
+                              perror("glob failed");
+                              
+                              free(filePath);
+                              
+                              closedir(dp);
+                              return -1;
+                        }
+                    }
+                }
+            }
+                
+        }
+    }
     closedir(dp);
     while (wait(NULL) > 0); // Wait for child processes to finish
     return 0;
- 
 }
+
 
 
 /**
@@ -162,43 +154,51 @@ int get_fileType(char *path, char* filename){
     
     struct stat statbuf;
     
+    // Open the directory
     dp = opendir(path);
     
+    // Check if directory can be opened
     if(dp == NULL){
         perror("opendir");
         return 0;
     }
     
+    // Get the status of the path
     if(stat(path, &statbuf) == -1){
          perror("stat failed");
          exit(EXIT_FAILURE);
          return 0;
      }
     
+    // Loop through the entries in the directory
     while((entry = readdir(dp))){
         if(stat(path, &statbuf) == -1)
             continue;
     
         FILE *fp1;
 
+        // Skip hidden files
 	if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
 	    continue;
 	}
 	
+	// Constructing the filePath
 	char *filePath = (char*) malloc((strlen(path)+1) + (strlen(entry->d_name)+1));
 	
 	filePath = strcpy(filePath, path);
 	strcat(filePath, "/");
 	strcat(filePath, entry->d_name);
 	
+	// Obtain file type from a file
 	int filetypeFromTest = fileSystemTests(filePath);
 	
-	
+	    // Traverse through the directory if its not the matched directory
 	    if(filetypeFromTest == 1 && strcmp(entry->d_name, filename) != 0){
 	
                 get_fileType(filePath, filename);
 	    }
 	
+	    // Else, print out the matched filepaths
 	    else{
 	    
 	        fp1 = fopen(filePath, "r");
@@ -273,7 +273,6 @@ void printPath(char* path){
 
 }
 
-
 /**
   *@brief manipulating input string for convenience
   *@param s, c
@@ -291,40 +290,3 @@ void replaceStr(char* s, char* c){
 		}
           }
 }
-
-
-
-
-
-int globFunct(char* pattern){
-	char **found;
-	glob_t gstruct;
-	int r;
-
-    r = glob(pattern, GLOB_ERR , NULL, &gstruct);
-    //printf("PATTERN INPUT IS %s\n", pattern);
-    //printf("VALUE r IS %i\n", r);
-    //check for errors
-    if( r!=0 )
-    {
-        if( r==GLOB_NOMATCH )
-            fprintf(stderr,"No matches\n");
-        else
-            fprintf(stderr,"Some kinda glob error\n");
-        exit(1);
-    }
-
-    // success, output found filenames
-    //printf("Found %zu filename matches\n",gstruct.gl_pathc);
-    found = gstruct.gl_pathv;
-    while(*found)
-    {
-        //printf("%s\n",*found);
-        found++;
-    }
-
-    return(0);
-}
-
-
-
